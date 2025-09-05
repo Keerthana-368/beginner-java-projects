d1b00be679bc22094f43d6b924ecfd5aa572f128
Keerthana-368 <keerthanamr58@gmail.com>
2025-09-05 20:59:01 +0530

    Add files via upload

---
+++ Firstcode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Firstcode {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please,enter your  name:");
+    String random=input.nextLine();
+    System.out.println("heyyy," + random +" " +"welcome to java tutorial");
+
+
+
+  }
+  
+}
